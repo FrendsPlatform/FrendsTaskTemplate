0e7e6287f1a39042233844ad060396e2aa0b3001
Peetu Saarinen <root@chargedbyte.dev>
2025-05-22 12:35:34 +0300

    Add missing documentation examples

--- Frends.Template/Frends.Echo.Execute/Definitions/Options.cs
+++ Frends.Template/Frends.Echo.Execute/Definitions/Options.cs
@@ -19,12 +19,14 @@ public class Options
     /// <summary>
     /// Whether to throw an error on failure.
     /// </summary>
+    /// <example>false</example>
     [DefaultValue(true)]
     public bool ThrowErrorOnFailure { get; set; }
 
     /// <summary>
     /// Overrides the error message on failure.
     /// </summary>
+    /// <example>Custom error message</example>
     [DisplayFormat(DataFormatString = "Text")]
     [DefaultValue("")]
     public string ErrorMessageOnFailure { get; set; }
