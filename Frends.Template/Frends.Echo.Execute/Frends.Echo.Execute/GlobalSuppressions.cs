256bf2305380dd7879e457d52401d344d8bbae28
Mateusz Noga-Wojtania <mateusz.noga.wojtania@gmail.com>
2025-12-19 16:49:40 +0100

    add suppressions

--- Frends.Template/Frends.Echo.Execute/Frends.Echo.Execute/GlobalSuppressions.cs
+++ Frends.Template/Frends.Echo.Execute/Frends.Echo.Execute/GlobalSuppressions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:PrefixLocalCallsWithThis", Justification = "Following Frends documentation guidelines")]
 [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:UsingDirectivesMustBePlacedWithinNamespace", Justification = "Following Frends documentation guidelines")]
 [assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:BracesMustNotBeOmitted", Justification = "Following Frends documentation guidelines")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Documentation checked by custom analyzers")]
